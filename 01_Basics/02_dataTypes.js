f0929fceb84f749a8bd5df7c9b728f2038cf9e17
Mumtaz Ali <mumtazalipintto@gmail.com>
2026-03-14 19:32:51 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // it works well on browser but not in nodejs
+
+console.log(3
+    +
+    3) // code readability should be hight
+console.log("Ali Mumtaz")
+
+let name = "Ali" // string data type 
+let age = 18 // number
+let isLoggedIn = false // boolean
+let state; // undefiend 
+
+// number => 2 to power 53
+// bigint 
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined) // undefiend
